8a229be6298cd4110414bf8b99d0295bcd00ac80
melina.ayala <74684055+creativenchaotic@users.noreply.github.com>
2023-11-29 18:47:31 +0000

    updated ParticleData struct

--- Coursework/Coursework/SPH_Particle.h
+++ Coursework/Coursework/SPH_Particle.h
@@ -3,13 +3,20 @@
 
 struct ParticleData {
     int size;
-    XMFLOAT3 padding = XMFLOAT3(0.f,0.f,0.f);
     XMFLOAT3 startPosition = XMFLOAT3(0.0f,0.0f,0.0f);
+
+    XMFLOAT3 currentPosition = XMFLOAT3(0.0f, 0.0f, 0.0f);
     float density = 0.0f;
-    XMFLOAT3 currentPosition = XMFLOAT3(0.0f,0.0f,0.0f);
-    float mass = 0.0f;
-    XMFLOAT3 velocity = XMFLOAT3(0.0f, 0.0f, 0.0f);;
-    float bounceDampingFactor =0.0f;
+
+    XMFLOAT3 predictedPosition = XMFLOAT3(0.0f,0.0f,0.0f);
+    float nearDensity = 0.0f;
+
+    XMFLOAT3 velocity = XMFLOAT3(0.0f, 0.0f, 0.0f);
+    int spatialOffsets = 0;
+
+    XMFLOAT3 spatialIndices = XMFLOAT3(0.0f,0.0f,0.0f);
+    float padding = 0.0f;
+
 };
 
 class SPH_Particle :
