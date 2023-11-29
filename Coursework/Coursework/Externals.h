f9e602ca4f77b2523ca43bec00798f805a5eb1b0
melina.ayala <74684055+creativenchaotic@users.noreply.github.com>
2023-11-29 15:23:11 +0000

    Adding structs to Externals header

--- Coursework/Coursework/Externals.h
+++ Coursework/Coursework/Externals.h
@@ -3,3 +3,44 @@
 enum class RenderSettings {
 	RenderColours, WorldPosition, Normals
 };
+
+struct SPHSimulationValues {
+	int numParticles;
+	int numParticlesPerAxis;
+	int particleResolution;
+	float particleSpacing;
+	int particleScale;
+
+	float gravity = -10.f;
+	float collisionDamping = 0.05f;
+	float smoothingRadius = 0.2f;
+	float targetDensity;
+	float pressureMultiplier;
+	float nearPressureMultiplier;
+	float viscosityStrength;
+
+	float timeScale = 1.f;
+	bool fixedTimeStep;
+	int iterationsPerFrame;
+};
+
+struct LightValues {
+	XMFLOAT4 lightColour;
+	XMFLOAT4 lightAmbientColour;
+	XMFLOAT3 lightPosition;
+	XMFLOAT3 lightDirection;
+	XMFLOAT4 lightSpecularColour;
+};
+
+struct PBRMaterialValues {
+	float materialRoughness;
+	float metallicFactor;
+	float baseReflectivity;
+};
+
+struct GUISettings {
+	bool hideInstructions;
+	bool displaySPHSimulationParticles;
+	bool displayWaterSurface;
+	bool isLightOn;
+};
