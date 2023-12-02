af05c5eedff0cea855a2e8ea9d5d251790ca4909
melina.ayala <74684055+creativenchaotic@users.noreply.github.com>
2023-12-02 21:28:30 +0000

    Setting up compute shader hlsl for bitonic mergesort

--- Coursework/Coursework/BitonicMergesort.hlsl
+++ Coursework/Coursework/BitonicMergesort.hlsl
@@ -1,4 +1,22 @@
-[numthreads(1, 1, 1)]
-void main( uint3 DTid : SV_DispatchThreadID )
+static const int NumThreads = 128;
+
+struct Particle
+{
+    int size;
+    float3 startPosition;
+    float3 currentPosition;
+    float density;
+    float3 predictedPosition;
+    float nearDensity;
+    float3 velocity;
+    int spatialOffsets;
+    float3 spatialIndices;
+    float padding;
+};
+
+RWStructuredBuffer<Particle> particleData : register(u0); //Data we pass to and from the compute shader
+
+[numthreads(NumThreads, 1, 1)]
+void main(uint3 groupThreadID : SV_GroupThreadID, int3 dispatchThreadID : SV_DispatchThreadID)
 {
 }
