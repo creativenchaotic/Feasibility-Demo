d8e8e1a697f6abcae6ffa9ad60b3cd7dda2d2277
melina.ayala <74684055+creativenchaotic@users.noreply.github.com>
2023-12-03 21:43:38 +0000

    updated offsets calculations to use the right buffers

--- Coursework/Coursework/OffsetCalculationComputeShader.hlsl
+++ Coursework/Coursework/OffsetCalculationComputeShader.hlsl
@@ -14,27 +14,14 @@ struct Particle
     float padding;
 };
 
-RWStructuredBuffer<Particle> particleData : register(u0); //Data we pass to and from the compute shader
-StructuredBuffer<Particle> particleDataOutputFromBitonicMergesort : register(t0);
+RWStructuredBuffer<int> particleOffsets : register(u0); //Data we pass to and from the compute shader
+StructuredBuffer<int3> particleIndicesOutputFromBitonicMergesort : register(t0);
 
 
 cbuffer cb_offsetCalculationsConstants : register(b0)
 {
     int numParticles;
     float3 padding;
-}
-
-void settingParticleDataForNextComputeShader(int3 thread)
-{
-    particleData[thread.x].currentPosition = particleDataOutputFromBitonicMergesort[thread.x].currentPosition;
-    particleData[thread.x].density = particleDataOutputFromBitonicMergesort[thread.x].density;
-    particleData[thread.x].nearDensity = particleDataOutputFromBitonicMergesort[thread.x].nearDensity;
-    particleData[thread.x].padding = particleDataOutputFromBitonicMergesort[thread.x].padding;
-    particleData[thread.x].predictedPosition = particleDataOutputFromBitonicMergesort[thread.x].predictedPosition;
-    particleData[thread.x].size = particleDataOutputFromBitonicMergesort[thread.x].size;
-    particleData[thread.x].spatialIndices = particleDataOutputFromBitonicMergesort[thread.x].spatialIndices;
-    particleData[thread.x].startPosition = particleDataOutputFromBitonicMergesort[thread.x].startPosition;
-    particleData[thread.x].velocity = particleDataOutputFromBitonicMergesort[thread.x].velocity;
 }
 
 [numthreads(NumThreads, 1, 1)]
@@ -46,13 +33,11 @@ void main(uint3 groupThreadID : SV_GroupThreadID, int3 dispatchThreadID : SV_Dis
     }
     uint i = dispatchThreadID.x;
 
-    uint key = particleDataOutputFromBitonicMergesort[i].spatialIndices.z;
-    uint keyPrev = i == 0 ? 9999999 : particleDataOutputFromBitonicMergesort[i - 1].spatialIndices.z;
+    uint key = particleIndicesOutputFromBitonicMergesort[i].z;
+    uint keyPrev = i == 0 ? 9999999 : particleIndicesOutputFromBitonicMergesort[i - 1].z;
     if (key != keyPrev)
     {
-        particleData[key].spatialOffsets = i;
+        particleOffsets[key] = i;
     }
-    
-    settingParticleDataForNextComputeShader(dispatchThreadID);
 
 }
