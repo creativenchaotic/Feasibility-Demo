c21ba43cecbb142214d04db9988089b443f32cb9
melina.ayala <74684055+creativenchaotic@users.noreply.github.com>
2023-12-03 17:43:41 +0000

    updated offset calculations shader to work with all particle data instead of just offset data

--- Coursework/Coursework/OffsetCalculationComputeShader.cpp
+++ Coursework/Coursework/OffsetCalculationComputeShader.cpp
@@ -30,18 +30,18 @@ void OffsetCalculationComputeShader::setShaderParameters(ID3D11DeviceContext* dc
 	dc->CSSetUnorderedAccessViews(0, 1, &offsetCalculationsOutputWritable, 0);//Same as UAVs
 }
 
-void OffsetCalculationComputeShader::createOutputUAVs(ID3D11Device* pd3dDevice, int numParticles, std::vector<int>* particlesSpatialOffsets)
+void OffsetCalculationComputeShader::createOutputUAVs(ID3D11Device* pd3dDevice, int numParticles, std::vector<ParticleData>* particles)
 {
     //Creating a buffer to output the data from the compute shader
     D3D11_BUFFER_DESC bufferDesc = {};//Creating a buffer description to create the buffer from
-    bufferDesc.ByteWidth = numParticles * sizeof(int);//sizeofT should be the particle data struct. Setting the size of the buffer to whatever amount is needed
+    bufferDesc.ByteWidth = numParticles * sizeof(ParticleData);//sizeofT should be the particle data struct. Setting the size of the buffer to whatever amount is needed
     bufferDesc.Usage = D3D11_USAGE_DEFAULT;
     bufferDesc.BindFlags = D3D11_BIND_UNORDERED_ACCESS | D3D11_BIND_SHADER_RESOURCE;//Setting how the buffer works
     bufferDesc.MiscFlags = D3D11_RESOURCE_MISC_BUFFER_STRUCTURED;
-    bufferDesc.StructureByteStride = sizeof(int);//make this the size of particle data//Setting the stride that the buffer needs to take when reading each element from memory
+    bufferDesc.StructureByteStride = sizeof(ParticleData);//make this the size of particle data//Setting the stride that the buffer needs to take when reading each element from memory
     D3D11_SUBRESOURCE_DATA bufferInitData;
-    bufferInitData.pSysMem = particlesSpatialOffsets->data();//Initial data that is getting passed into the buffer
-    pd3dDevice->CreateBuffer(&bufferDesc, (particlesSpatialOffsets) ? &bufferInitData : nullptr, &offsetCalculationsOutput);//Creates the buffer
+    bufferInitData.pSysMem = particles->data();//Initial data that is getting passed into the buffer
+    pd3dDevice->CreateBuffer(&bufferDesc, (particles) ? &bufferInitData : nullptr, &offsetCalculationsOutput);//Creates the buffer
 
     // Create SRV - Lets you read from the Buffer created
     D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
