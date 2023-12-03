d952b2852cdf2bae3abdfa664b97375b381659ac
melina.ayala <74684055+creativenchaotic@users.noreply.github.com>
2023-12-03 00:47:41 +0000

    Deleted unnecessary functions and buffer in sph compute shader first pass cpp and header

--- Coursework/Coursework/ComputeShader.h
+++ Coursework/Coursework/ComputeShader.h
@@ -13,7 +13,6 @@ public:
 
 	void setShaderParameters(ID3D11DeviceContext* dc);
 	void createOutputUAVs(ID3D11Device* pd3dDevice, int numParticles, std::vector<ParticleData>* particles);
-	void createBuffer(ID3D11Device* pd3dDevice, int numParticles, std::vector<ParticleData>* particles);
 	void setSimulationConstants(ID3D11DeviceContext* dc, int numParticlesVal, float gravityVal, float delta, float bounceDamping,float smoothingRadiusVal, float targetDensityVal,float pressureMultiplierVal, float nearPressureMultVal, float viscosity, float edgeForceVal, float edgeForceDistanceVal, float bb_Top,float bb_Bottom,float bb_LeftSide, float bb_rightSide, float bb_Back, float bb_front);//Used to pass in constant variables such as gravity or damping values
 	void unbind(ID3D11DeviceContext* dc);
 	ID3D11ShaderResourceView* getComputeShaderOutput() { return particlesOutputReadable; };
@@ -21,13 +20,6 @@ public:
 
 private:
 	void initShader(const wchar_t* cfile, const wchar_t* blank);
-
-	void createPositionsBuffer(ID3D11Device* pd3dDevice, int numParticles);
-	void createPredictedPositionsBuffer(ID3D11Device* pd3dDevice, int numParticles);
-	void createVelocityBuffer(ID3D11Device* pd3dDevice, int numParticles);
-	void createDensityBuffer(ID3D11Device* pd3dDevice, int numParticles);
-	void createSpatialIndicesBuffer(ID3D11Device* pd3dDevice, int numParticles);
-	void createSpatialOffsetsBuffer(ID3D11Device* pd3dDevice, int numParticles);
 
 	struct SimulationConstantsBufferType {
 		int numParticles;
@@ -55,46 +47,10 @@ private:
 		XMFLOAT2 padding2 = XMFLOAT2(0.0f,0.0f);
 	};
 
-
-
-	//Buffer to input data into the compute shader------------------------
-	ID3D11Buffer* particlesComputeShaderInput;
-	ID3D11ShaderResourceView* particlesComputeShaderInputSRV;
-
 	//RW Structured Buffers-----------------------------------------------
 	ID3D11Buffer* particlesComputeShaderOutput;
 	ID3D11ShaderResourceView* particlesOutputReadable;
 	ID3D11UnorderedAccessView* particlesOutputWritable;
-
-	//Particle Positions
-	ID3D11Buffer* particlePositionBuffer;
-	ID3D11ShaderResourceView* particlePositionBufferSRV;
-	ID3D11UnorderedAccessView* particlePositionBufferUAV;
-
-	//Particle Predicted Positions
-	ID3D11Buffer* particlePredictedPositionBuffer;
-	ID3D11ShaderResourceView* particlePredictedPositionBufferSRV;
-	ID3D11UnorderedAccessView* particlePredictedPositionBufferUAV;
-
-	//Particle Velocities
-	ID3D11Buffer* particleVelocityBuffer;
-	ID3D11ShaderResourceView* particleVelocityBufferSRV;
-	ID3D11UnorderedAccessView* particleVelocityBufferUAV;
-
-	//Particle Densities
-	ID3D11Buffer* particleDensityBuffer;
-	ID3D11ShaderResourceView* particleDensityBufferSRV;
-	ID3D11UnorderedAccessView* particleDensityBufferUAV;
-
-	//Particle Spatial Indices
-	ID3D11Buffer* spatialIndicesBuffer;
-	ID3D11ShaderResourceView* spatialIndicesBufferSRV;
-	ID3D11UnorderedAccessView* spatialIndicesBufferUAV;
-
-	//Particle Spatial Offsets
-	ID3D11Buffer* spatialOffsetsBuffer;
-	ID3D11ShaderResourceView* spatialOffsetsBufferSRV;
-	ID3D11UnorderedAccessView* spatialOffsetsBufferUAV;
 
 	//Constant Buffer-----------------------------------------------------
 	ID3D11Buffer* simulationConstantsBuffer;
