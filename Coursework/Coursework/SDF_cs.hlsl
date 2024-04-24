9f012e8824cb654a15071bb2035d06dcc5f08422
melina.ayala <creativenchaotic@gmail.com>
2024-04-24 13:19:22 +0100

    Changing num threads in sdf texture3D compute shader

--- Coursework/Coursework/SDF_cs.hlsl
+++ Coursework/Coursework/SDF_cs.hlsl
@@ -3,6 +3,8 @@
 static const int NumThreads = 64;
 
 RWStructuredBuffer<float4> particleData : register(u0); //Data we pass to and from the compute shader. Currently the particle data, this should probably be an SRV instead since we are wanting to output the results of the SDF from this shader.
+RWTexture3D<snorm float> SDFImage : register(u1);
+
 
 cbuffer cb_simConstants : register(b0){
     int numParticles;
@@ -50,40 +52,9 @@ float sdfCalculations(float3 position)
     return finalValue;
 }
 
-[numthreads(1, 1, 1)]
-void main( uint3 DTid : SV_DispatchThreadID )
+[numthreads(32, 32, 1)]
+void main( uint3 DTid : SV_DispatchThreadID, uint3 groupId : SV_GroupID, uint3 GroupThreadID : SV_GroupThreadID, int groupIndex : SV_GroupIndex )
 {
-    /*
-	//Setting up colours for SDF
-    float3 finalColour = float3(0, 0, 0);
-    float4 posColour = float4(0.0, 0.0, 1.0f, 1.0f);
-    float4 negColour = float4(1.0, 0.0, 0.0, 1.0f);
-
-    //Initialising variables used for raymarching
-    float3 rayOrigin = float3(0, 0, -10.f);
-    //float3 rayOrigin = cameraPos;
-    float3 rayDirection = normalize(float3(input.tex, 1)); //Sets the direction of the ray to each point in the plane based on UVs
-    float totalDistanceTravelled = 0.f; //Total distance travelled by ray from the camera's position
-
-
-    //Raymarching (Sphere tracing)
-
-    for (int i = 0; i < 100; i++)//The number of steps affects the quality of the results and the performance
-    {
-        float3 positionInRay = rayOrigin + rayDirection * totalDistanceTravelled; //Current position along the ray based on the distance from the rays origin
-
-        float distanceToScene = sdfCalculations(positionInRay); //Current distance to the scene. Safe distance the point can travel to in any direction without overstepping an object
-
-        totalDistanceTravelled += distanceToScene;
-
-         //Colouring
-        finalColour = float3(totalDistanceTravelled, totalDistanceTravelled, totalDistanceTravelled) / 100;
-
-        if (distanceToScene < 0.001f || totalDistanceTravelled > 100.f)//If the distance to an SDF shape becomes smaller than 0.001 stop iterating //Stop iterating if the ray moves too far without hitting any objects
-        {
-            break;
-        }
-
-    }
-*/
+    uint3 resolution;
+    SDFImage.GetDimensions(resolution.x, resolution.y, resolution.z);
 }
