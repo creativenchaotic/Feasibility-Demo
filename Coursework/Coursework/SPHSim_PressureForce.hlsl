413a0d8561425f61d2bfeb09e555147fc6a619b1
melina.ayala <creativenchaotic@gmail.com>
2024-05-03 20:41:43 +0100

    Moved Pressure force functions to its own compute shader

--- Coursework/Coursework/SPHSim_PressureForce.hlsl
+++ Coursework/Coursework/SPHSim_PressureForce.hlsl
@@ -1,6 +1,5 @@
 
 //compute shader used for SPH simulation
-
 static const int NumThreads = 64;
 
 struct Particle
@@ -14,10 +13,6 @@ struct Particle
     uint spatialOffsets;
 };
 
-RWStructuredBuffer<Particle> particleData : register(u0); //Data we pass to and from the compute shader
-StructuredBuffer<Particle> sphSimulationFirstPassOutput : register(t0);
-StructuredBuffer<uint3> bitonicMergesortParticleIndicesOutput : register(t1);
-StructuredBuffer<uint> particleOffsetCalculationsOutput : register(t2);
 
 cbuffer cb_simConstants : register(b0)
 {
@@ -48,6 +43,9 @@ cbuffer cb_simConstants : register(b0)
     float4x4 localToWorld;
     float4x4 worldToLocal;
 };
+
+RWStructuredBuffer<Particle> particleData : register(u0); //Data we pass to and from the compute shader
+StructuredBuffer<Particle> sphDensityPassOutput : register(t0);
 
 
 //SPATIAL 3D HASH----------------------------------------------------------------
@@ -109,39 +107,6 @@ uint KeyFromHash(uint hash, uint tableSize)
 
 static const float PI = 3.1415926f;
 
-float SpikyKernelPow2(float dst, float radius)
-{
-    if (dst < radius)
-    {
-        float scale = 15 / (2 * PI * pow(radius, 5));
-        float v = radius - dst;
-        return v * v * scale;
-    }
-    return 0;
-}
-
-float DensityKernel(float dst, float radius)
-{
-	//return SmoothingKernelPoly6(dst, radius);
-    return SpikyKernelPow2(dst, radius);
-}
-
-float SpikyKernelPow3(float dst, float radius)
-{
-    if (dst < radius)
-    {
-        float scale = 15 / (PI * pow(radius, 6));
-        float v = radius - dst;
-        return v * v * v * scale;
-    }
-    return 0;
-}
-
-float NearDensityKernel(float dst, float radius)
-{
-    return SpikyKernelPow3(dst, radius);
-}
-
 float PressureFromDensity(float density)
 {
     return (density - targetDensity) * pressureMultiplier;
@@ -152,16 +117,54 @@ float NearPressureFromDensity(float nearDensity)
     return nearDensity * nearPressureMultiplier;
 }
 
-void CalculateDensities(uint3 thread)
+float DerivativeSpikyPow2(float dst, float radius)
+{
+    if (dst <= radius)
+    {
+        float scale = 15 / (pow(radius, 5) * PI);
+        float v = radius - dst;
+        return -v * scale;
+    }
+    return 0;
+}
+
+float DensityDerivative(float dst, float radius)
+{
+    return DerivativeSpikyPow2(dst, radius);
+}
+
+float DerivativeSpikyPow3(float dst, float radius)
+{
+    if (dst <= radius)
+    {
+        float scale = 45 / (pow(radius, 6) * PI);
+        float v = radius - dst;
+        return -v * v * scale;
+    }
+    return 0;
+}
+
+float NearDensityDerivative(float dst, float radius)
+{
+    return DerivativeSpikyPow3(dst, radius);
+}
+
+//SIMULATION FUNCTIONS---------------------------------------------------------
+void CalculatePressureForce(uint3 thread)
 {
     if (thread.x >= numParticles)
         return;
 
+	// Calculate pressure
+    float density = particleData[thread.x].density[0];
+    float densityNear = particleData[thread.x].density[1];
+    float pressure = PressureFromDensity(density);
+    float nearPressure = NearPressureFromDensity(densityNear);
+    float3 pressureForce = 0;
+	
     float3 pos = particleData[thread.x].predictedPosition;
     int3 originCell = GetCell3D(pos, smoothingRadius);
     float sqrRadius = smoothingRadius * smoothingRadius;
-    float density = 0;
-    float nearDensity = 0;
 
 	// Neighbour search
     for (int i = 0; i < 27; i++)
@@ -182,6 +185,10 @@ void CalculateDensities(uint3 thread)
                 continue;
 
             uint neighbourIndex = indexData[0];
+			// Skip if looking at self
+            if (neighbourIndex == thread.x)
+                continue;
+
             float3 neighbourPos = particleData[neighbourIndex].predictedPosition;
             float3 offsetToNeighbour = neighbourPos - pos;
             float sqrDstToNeighbour = dot(offsetToNeighbour, offsetToNeighbour);
@@ -190,27 +197,35 @@ void CalculateDensities(uint3 thread)
             if (sqrDstToNeighbour > sqrRadius)
                 continue;
 
-			// Calculate density and near density
+			// Calculate pressure force
+            float densityNeighbour = particleData[neighbourIndex].density[0];
+            float nearDensityNeighbour = particleData[neighbourIndex].density[1];
+            float neighbourPressure = PressureFromDensity(densityNeighbour);
+            float neighbourPressureNear = NearPressureFromDensity(nearDensityNeighbour);
+
+            float sharedPressure = (pressure + neighbourPressure) / 2;
+            float sharedNearPressure = (nearPressure + neighbourPressureNear) / 2;
+
             float dst = sqrt(sqrDstToNeighbour);
-            density += DensityKernel(dst, smoothingRadius);
-            nearDensity += NearDensityKernel(dst, smoothingRadius);
+            float3 dir = dst > 0 ? offsetToNeighbour / dst : float3(0, 1, 0);
+
+            pressureForce += dir * DensityDerivative(dst, smoothingRadius) * sharedPressure / densityNeighbour;
+            pressureForce += dir * NearDensityDerivative(dst, smoothingRadius) * sharedNearPressure / nearDensityNeighbour;
         }
     }
-	
-    particleData[thread.x].density = float2(density, nearDensity);
+
+    float3 acceleration = pressureForce / density;
+    particleData[thread.x].velocity += acceleration * deltaTime;
 }
 
-void SetParticleDataOffsetsAndIndices(uint3 thread)
+void SetParticleValuesFromPreviousStage(uint3 thread)
 {
-    particleData[thread.x] = sphSimulationFirstPassOutput[thread.x];
-    particleData[thread.x].spatialIndices = bitonicMergesortParticleIndicesOutput[thread.x];
-    particleData[thread.x].spatialOffsets = particleOffsetCalculationsOutput[thread.x];
+    particleData[thread.x] = sphDensityPassOutput[thread.x];
 }
 
 [numthreads(NumThreads, 1, 1)]
 void main(uint3 groupThreadID : SV_GroupThreadID, uint3 dispatchThreadID : SV_DispatchThreadID)
 {
-    SetParticleDataOffsetsAndIndices(dispatchThreadID);
-    CalculateDensities(dispatchThreadID);
-  
+    SetParticleValuesFromPreviousStage(dispatchThreadID);
+    CalculatePressureForce(dispatchThreadID);
 }
